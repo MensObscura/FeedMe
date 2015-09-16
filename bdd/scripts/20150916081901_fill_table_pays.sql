1efea82f6f553795988d7db04ae4f0699a355c05
Maxime CLEMENT <maxime.clement93@gmail.com>
2015-09-16 10:44:15 +0200

    [ PAYS ] Ajout du getAll + TEST

--- bdd/scripts/20150916081901_fill_table_pays.sql
+++ bdd/scripts/20150916081901_fill_table_pays.sql
@@ -1,10 +1,11 @@
 -- // fill_table_pays
 -- Migration SQL that makes the change goes here.
 INSERT INTO PAYS (PAY_CODE_PAYS, PAY_NOM) VALUES
-('FR', 'France');
+('FR', 'France'),
+('BE', 'Belgique');
 
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-DELETE FROM PAYS WHERE PAY_ID = 1;
+DELETE FROM PAYS WHERE PAY_ID BETWEEN 1 AND 2;
 
--- src/main/java/fil/iagl/iir/dao/pays/PaysDao.java
+++ src/main/java/fil/iagl/iir/dao/pays/PaysDao.java
@@ -1,5 +1,7 @@
 package fil.iagl.iir.dao.pays;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import fil.iagl.iir.entite.Pays;
@@ -7,5 +9,7 @@ import fil.iagl.iir.entite.Pays;
 public interface PaysDao {
 
 	Pays getById(@Param("id") Integer id);
+
+	List<Pays> getAll();
 
 }
