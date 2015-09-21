0ea87b549ababd803d31578aec4e7a9548d175cf
Maxime CLEMENT <maxime.clement93@gmail.com>
2015-09-21 11:05:43 +0200

    [ Test ] Utilisateur de Mock dans les tests services afin d'isoler la couche DAO

--- src/test/java/fil/iagl/iir/service/AbstractServiceTest.java
+++ src/test/java/fil/iagl/iir/service/AbstractServiceTest.java
@@ -1,12 +1,34 @@
 package fil.iagl.iir.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import fil.iagl.iir.AbstractFeedMeTest;
+import fil.iagl.iir.dao.utilisateur.UtilisateurDao;
+import fil.iagl.iir.entite.Utilisateur;
+import fil.iagl.iir.service.impl.UtilisateurServiceImpl;
 
-public abstract class AbstractServiceTest extends AbstractFeedMeTest{
+public abstract class AbstractServiceTest extends AbstractFeedMeTest {
 
-	@Autowired
-	protected UtilisateurService utilisateurService;
-	
+	@InjectMocks
+	protected UtilisateurServiceImpl utilisateurService;
+
+	@Mock
+	protected UtilisateurDao utilisateurDao;
+
+	@Before
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	protected Utilisateur createUtilisateur() {
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setIdUtilisateur(1);
+		utilisateur.setMail("toto.toto@gmail.com");
+		utilisateur.setNom("toto");
+		return utilisateur;
+	}
+
 }
--- src/test/java/fil/iagl/iir/service/utilisateur/UtilisateurServiceTest.java
+++ src/test/java/fil/iagl/iir/service/utilisateur/UtilisateurServiceTest.java
@@ -2,22 +2,35 @@ package fil.iagl.iir.service.utilisateur;
 
 import org.fest.assertions.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import fil.iagl.iir.entite.Utilisateur;
 import fil.iagl.iir.service.AbstractServiceTest;
 
-
 public class UtilisateurServiceTest extends AbstractServiceTest {
-	
-	
-	
+
 	@Test
 	public void getByIdTestSucces() throws Exception {
 		Integer id = 1;
-		Assertions.assertThat(utilisateurService.getById(id)).isNotNull();
+
+		Utilisateur mockUtilisateur = this.createUtilisateur();
+		Mockito.when(utilisateurDao.getById(id)).thenReturn(mockUtilisateur);
+
+		Utilisateur utilisateur = utilisateurService.getById(id);
+
+		Assertions.assertThat(utilisateur).isNotNull();
+		Assertions.assertThat(utilisateur.getIdUtilisateur()).isNotNull().isEqualTo(mockUtilisateur.getIdUtilisateur());
+		Assertions.assertThat(utilisateur.getNom()).isNotNull().isEqualTo(mockUtilisateur.getNom());
+		Assertions.assertThat(utilisateur.getMail()).isNotNull().isEqualTo(mockUtilisateur.getMail());
+
+		Mockito.verify(utilisateurDao, Mockito.times(1)).getById(id);
+
 	}
-	
-	@Test(expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void getByIdTestEchec() throws Exception {
 		utilisateurService.getById(null);
+		Mockito.verify(utilisateurDao, Mockito.times(0)).getById(null);
 	}
+
 }
