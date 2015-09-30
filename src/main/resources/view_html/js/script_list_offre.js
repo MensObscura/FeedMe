37edd4fa29a4963b0b79c8603307f6e7998e7d66
Rosa Thibault <rosa@b10p10.fil.univ-lille1.fr>
2015-09-30 13:52:45 +0200

    offres

--- src/main/resources/view_html/js/script_list_offre.js
+++ src/main/resources/view_html/js/script_list_offre.js
@@ -1,15 +1,80 @@
 var app = angular.module("ListApp", []);
+var page_data;
 
-app.controller("ListCtrl", function($scope, $http) {
-	$http.get('http://localhost:8080/offres').success(
+app.controller("ListCtrl", function($scope, $http, $location,srvShareData) {
+	/*$http.get('http://localhost:8080/offres').success(*/
+	$http.get('ressources/js/list.json').success(
 		function(data) {
 			$scope.list = data;
 		}
 	);
 	
-	$scope.visualize = function(item) {
+$scope.dataToShare = [];
+  
+  $scope.visualize = function (myValue) {
 
-    	alert(item.titre);
-    	
+    $scope.dataToShare = JSON.stringify(myValue);
+    srvShareData.addData($scope.dataToShare);
+    
+    window.location.href = "offre.html";
   }
+});
+
+app.service('srvShareData', function($window) {
+        var KEY = 'App.SelectedValue';
+
+        var addData = function(newObj) {
+            var mydata = newObj
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            } else {
+                mydata = [];
+            }
+          
+            $window.sessionStorage.setItem(KEY, JSON.stringify(mydata));
+        };
+
+        var getData = function(){
+            var mydata = $window.sessionStorage.getItem(KEY);
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            }
+            return mydata || [];
+        };
+
+        return {
+            addData: addData,
+            getData: getData
+        };
+    });
+
+
+app.controller('ReservationController', function($scope, $http, srvShareData) {  
+	
+	$scope.offre = srvShareData.getData();
+	
+  $scope.submitForm = function() {
+    if ($scope.ReservationForm.$valid) {
+     
+    	 var data = {
+		    	nombre : $scope.place,
+		    };
+
+		  $http({
+			url: 'http://localhost:8080/reservations',dataType: 'json',method: 'POST',data: data}
+
+			).success(function(response)
+			{
+				$scope.response = response;
+			}
+			).error(function(error)
+			{
+				$scope.error = error;
+			})
+
+
+    }
+
+  };
+  
 });
