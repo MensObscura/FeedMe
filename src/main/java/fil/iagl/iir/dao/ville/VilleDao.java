c569ae964c586eb2237e9f2bb09b7052267769e3
Maxime CLEMENT <maxime.clement93@gmail.com>
2015-10-13 18:54:05 +0200

    [ Parametrage ] GET /settings/pays & /settings/typescuisines

--- src/main/java/fil/iagl/iir/dao/ville/VilleDao.java
+++ src/main/java/fil/iagl/iir/dao/ville/VilleDao.java
@@ -5,9 +5,9 @@ import org.apache.ibatis.annotations.Param;
 import fil.iagl.iir.entite.Ville;
 
 public interface VilleDao {
-	
+
 	public Ville getById(@Param("id") Integer id);
-	
+
 	public Integer sauvegarder(@Param("ville") Ville ville);
 
 }
