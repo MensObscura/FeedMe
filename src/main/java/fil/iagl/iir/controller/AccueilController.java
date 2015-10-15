c80a4c7cb3935eb5ebcc8d350c74565a86aa9e90
Thibaud <thibaud.verbaere@gmail.com>
2015-10-15 16:14:09 +0200

    changement redirection accueil

--- src/main/java/fil/iagl/iir/controller/AccueilController.java
+++ src/main/java/fil/iagl/iir/controller/AccueilController.java
@@ -9,7 +9,7 @@ public class AccueilController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "redirect:/resources/index.html";
+		return "redirect:index.html";
 	}
 
 }
