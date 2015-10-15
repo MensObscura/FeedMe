e1f4b757990b24541d71859e5aaf7c696723a23d
Thibaud <thibaud.verbaere@gmail.com>
2015-10-15 15:59:44 +0200

    clean liste offres

--- src/main/resources/public/resources/js/script_list_offre.js
+++ src/main/resources/public/resources/js/script_list_offre.js
@@ -1,19 +1,20 @@
-var app = angular.module("ListApp", []);
+// Chargement du module "ListeApp"
+var app = angular.module("ListeApp", []);
 
-app.controller("ListCtrl", function($scope, $http, $location) {
+// Création du controller "ListeCtrl"
+app.controller("ListeCtrl", function($scope, $http, $location) {
+	// On se connecte à la route consacrée pour récupèrer les offres
 	$http.get('http://localhost:8080/offres').success(
 		function(data) {
 			$scope.list = data;
 		}
 	);
 	
-	
-	  $scope.visualize = function (myValue) {
-		   
-		    window.location.href = "offre.html?id="+myValue.id;
-		  };
+	// Permet de créer un listener qui va rediriger vers la visualisation de l'offre cliquée
+	$scope.visualize = function (valeur) {
+		   window.location.href = "offre.html?id="+valeur.id;
+	};
   
-
 });
 
 
