44a2f996ebb5c8975cd5abd7e8f294e14691760c
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-10-19 21:45:53 +0200

    [ReservationDaoTest] refacto pour eviter duplication

--- src/test/java/fil/iagl/iir/dao/ReservationDaoTest.java
+++ src/test/java/fil/iagl/iir/dao/ReservationDaoTest.java
@@ -16,26 +16,17 @@ import fil.iagl.iir.outils.SQLCODE;
 
 public class ReservationDaoTest extends AbstractDaoTest {
 
+	private static final Integer OFFRE_ID_DEFAULT = 1;
+	private static final Integer CONVIVE_ID_DEFAULT = 2;
+	private static final LocalDate DATE_DEFAULT = LocalDate.of(2015, Month.JANUARY, 15);
+
 	private static final Integer OFFRE_ID_4 = 4;
 	private static final Integer NB_RESERVATION_OFFRE_ID_4 = 2;
 
 	@Test
 	public void sauvegarderTestSucces() throws Exception {
-		// Etant donne une reservation associee a une offre et un convive
-		Integer idOffre = 1;
-		Integer idConvive = 2;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
-
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		// Etant donne une reservation associee a une offre et a un convive
+		Reservation reservation = buildReservation(OFFRE_ID_DEFAULT, CONVIVE_ID_DEFAULT, DATE_DEFAULT);
 
 		// Quand on enregistre la reservation
 		this.reservationDao.sauvegarder(reservation);
@@ -44,27 +35,18 @@ public class ReservationDaoTest extends AbstractDaoTest {
 		Assertions.assertThat(reservation.getId()).isNotNull().isPositive();
 		// et que les informations sont conformes
 		Assertions.assertThat(reservation.getConvive()).isNotNull();
-		Assertions.assertThat(reservation.getConvive().getIdUtilisateur()).isNotNull().isEqualTo(idConvive);
+		Assertions.assertThat(reservation.getConvive().getIdUtilisateur()).isNotNull().isEqualTo(CONVIVE_ID_DEFAULT);
 		Assertions.assertThat(reservation.getOffre()).isNotNull();
-		Assertions.assertThat(reservation.getOffre().getId()).isNotNull().isEqualTo(idOffre);
-		Assertions.assertThat(reservation.getDateReservation()).isNotNull().isEqualTo(dateReservation);
+		Assertions.assertThat(reservation.getOffre().getId()).isNotNull().isEqualTo(OFFRE_ID_DEFAULT);
+		Assertions.assertThat(reservation.getDateReservation()).isNotNull().isEqualTo(DATE_DEFAULT);
 	}
 
 	@Test
 	public void sauvegarderTestEchec_OffreNull() throws Exception {
-		// Etant donne une reservation associee a aucune offre
-		Integer idConvive = 2;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
-
-		Offre offre = null;
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		// Etant donne une reservation
+		Reservation reservation = buildReservation(null, CONVIVE_ID_DEFAULT, DATE_DEFAULT);
+		// qui n'est associee a aucune offre
+		reservation.setOffre(null);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -79,19 +61,10 @@ public class ReservationDaoTest extends AbstractDaoTest {
 
 	@Test
 	public void sauvegarderTestEchec_ConviveNull() throws Exception {
-		// Etant donne une reservation associee a aucun convive
-		Integer idOffre = 1;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
-
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = null;
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		// Etant donne une reservation
+		Reservation reservation = buildReservation(OFFRE_ID_DEFAULT, null, DATE_DEFAULT);
+		// qui n'est associee a aucun convive
+		reservation.setConvive(null);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -107,20 +80,7 @@ public class ReservationDaoTest extends AbstractDaoTest {
 	@Test
 	public void sauvegarderTestEchec_DateReservationNull() throws Exception {
 		// Etant donne une reservation n'ayant aucune date de reservation
-		Integer idOffre = 1;
-		Integer idConvive = 2;
-		LocalDate dateReservation = null;
-
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		Reservation reservation = buildReservation(OFFRE_ID_DEFAULT, CONVIVE_ID_DEFAULT, null);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -137,20 +97,7 @@ public class ReservationDaoTest extends AbstractDaoTest {
 	public void sauvegarderTestEchec_OffreNonExistant() throws Exception {
 		// Etant donne une reservation associee a une offre qui n'existe pas en
 		// base
-		Integer idOffre = Integer.MAX_VALUE;
-		Integer idConvive = 2;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
-
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		Reservation reservation = buildReservation(Integer.MAX_VALUE, CONVIVE_ID_DEFAULT, DATE_DEFAULT);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -167,20 +114,7 @@ public class ReservationDaoTest extends AbstractDaoTest {
 	public void sauvegarderTestEchec_ConviveNonExistant() throws Exception {
 		// Etant donne une reservation associee a un convive qui n'existe pas en
 		// base
-		Integer idOffre = 1;
-		Integer idConvive = Integer.MAX_VALUE;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
-
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		Reservation reservation = buildReservation(OFFRE_ID_DEFAULT, Integer.MAX_VALUE, DATE_DEFAULT);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -195,21 +129,10 @@ public class ReservationDaoTest extends AbstractDaoTest {
 
 	@Test
 	public void sauvegarderTestEchec_TupleUnique() throws Exception {
-		// Etant donne une reservation qui existe deja en base
-		Integer idOffre = 1;
 		Integer idConvive = 1;
-		LocalDate dateReservation = LocalDate.of(2015, Month.JANUARY, 15);
 
-		Offre offre = new Offre();
-		offre.setId(idOffre);
-
-		Utilisateur convive = new Utilisateur();
-		convive.setIdUtilisateur(idConvive);
-
-		Reservation reservation = new Reservation();
-		reservation.setOffre(offre);
-		reservation.setConvive(convive);
-		reservation.setDateReservation(dateReservation);
+		// Etant donne une reservation qui existe deja en base
+		Reservation reservation = buildReservation(OFFRE_ID_DEFAULT, idConvive, DATE_DEFAULT);
 
 		try {
 			// Quand on enregistre cette reservation en base
@@ -252,5 +175,24 @@ public class ReservationDaoTest extends AbstractDaoTest {
 		// Quand on recupere une reservation avec cet ID
 		// Alors la reservation retournee est nulle
 		Assertions.assertThat(this.reservationDao.getAllByIdOffre(Integer.MAX_VALUE)).isEmpty();
+	}
+
+	/* **************************** BUILDS ***********************************/
+
+	public Reservation buildReservation(Integer idOffre, Integer idConvive, LocalDate dateReservation) {
+		// Creation d'une offre
+		Offre offre = new Offre();
+		offre.setId(idOffre);
+
+		// Creation d'un convive
+		Utilisateur convive = new Utilisateur();
+		convive.setIdUtilisateur(idConvive);
+
+		Reservation reservation = new Reservation();
+		reservation.setOffre(offre);
+		reservation.setConvive(convive);
+		reservation.setDateReservation(dateReservation);
+
+		return reservation;
 	}
 }
