541c449821c277a60e5246f9d4ce5e55d7e90f46
piorunj <jordan.piorun@gmail.com>
2015-10-21 00:02:13 +0200

    ajout de javadoc pour les interfaces des services

--- src/main/java/fil/iagl/iir/service/ReservationService.java
+++ src/main/java/fil/iagl/iir/service/ReservationService.java
@@ -9,8 +9,19 @@ import fil.iagl.iir.entite.Reservation;
 @Service
 public interface ReservationService {
 
+	/**
+	 * Permet d'enregistrer une réservation
+	 * @param reservation La réservation a enregistrer
+	 * @return Le nombre de lignes insérées
+	 */
   Integer sauvegarder(Reservation reservation);
 
+  
+  /**
+   * Permet de récupérer toutes les réservations en rapport à une certaine offre
+   * @param offreId L'id de l'offre dont on veut récupérer les réservations
+   * @return Une liste de réservation
+   */
   List<Reservation> getAllReservationByOffre(Integer offreId);
 
 }
--- src/main/java/fil/iagl/iir/service/UtilisateurService.java
+++ src/main/java/fil/iagl/iir/service/UtilisateurService.java
@@ -8,8 +8,18 @@ import fil.iagl.iir.entite.Utilisateur;
 @Service
 public interface UtilisateurService {
 
+	/**
+	 * Permet de récupérer un utilisateur selon son ID
+	 * @param id L'ID de l'utilisateur a récupérer
+	 * @return Un utilisateur
+	 */
   Utilisateur getById(Integer id);
 
+  /**
+   * Permet de récupérer un particulier en fonction de l'ID utilisateur qui lui est associé 
+   * @param id L'ID de l'utilisateur
+   * @return Un particulier
+   */
   Particulier getParticulierByUtilisisateurId(Integer id);
 
 }
