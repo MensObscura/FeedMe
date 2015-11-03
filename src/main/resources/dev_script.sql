4bd221732b71213844fc301a6f497bf98bf1adcf
MensObscura <thibaultrosa@gmail.com>
2015-11-03 02:00:16 +0100

    Data dev_script.sql

--- src/main/resources/dev_script.sql
+++ src/main/resources/dev_script.sql
@@ -28,16 +28,16 @@ INSERT INTO VILLE(VIL_NOM, VIL_CP, VIL_PAY_ID) VALUES
 -- Table ADRESSE
 -- ---------------------------
 INSERT INTO ADRESSE(adr_voie, adr_vil_id) VALUES
-('45 Rue des poissons',1);
+('45 Rue des sacres',1);
 INSERT INTO ADRESSE(adr_voie, adr_vil_id) VALUES
 ('21 Avenue des champs',2);
 -- ---------------------------
 -- Table OFFRE
 -- ---------------------------
 INSERT INTO OFFRE(OFF_DATE_CREATION,OFF_TITRE,OFF_PRIX, OFF_NOMBRE_PERSONNE,OFF_DUREE_MINUTE, OFF_DATE_REPAS, OFF_NOTE, OFF_MENU, OFF_AGE_MIN, OFF_AGE_MAX, OFF_ANIMAUX, OFF_ADR_ID, OFF_TYP_ID, OFF_USR_ID) VALUES
-('2012-10-13','Chocolat Farcis',12,5,123,'2012-10-19 03:14:07','Chocolat noir et bio','Farce de foie de canard dans son coufin de chocolat',10,25,'true',1,1,1);
+('2012-10-13','Tomates nordique',12,5,123,'2012-10-19 03:14:07','Des tomates délicieuses','Entrée : Salade composée. Plat Principal : Tomates Farcies, riz à la crème fraiche et au vin blanc. Dessert : Poires belle Hélène',10,45,'true',1,1,1);
 
 INSERT INTO OFFRE(OFF_DATE_CREATION,OFF_TITRE,OFF_PRIX, OFF_NOMBRE_PERSONNE,OFF_DUREE_MINUTE, OFF_DATE_REPAS, OFF_NOTE, OFF_MENU, OFF_AGE_MIN, OFF_AGE_MAX, OFF_ANIMAUX, OFF_ADR_ID, OFF_TYP_ID, OFF_USR_ID) VALUES
-('2012-10-13','Pintade aux marrons',19,4,168,'2012-10-19 03:14:07','RAS','Des marrons et de la pintade',11,29,'true',2,1,1);
+('2012-10-13','Poulet subsaharien',19,4,168,'2012-10-19 03:14:07','Attention les arachides sont un allergène','Entrée : Buffet froid ,Plat : Poulet aux arachides, riz, carottes, piment. Dessert : Kakis et autres fruits exotiques',18,29,'true',2,1,1);
 
 
