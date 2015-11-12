2311ce1f2a00babfdfd978e1edc08ca3e3ad51d1
MensObscura <thibaultrosa@gmail.com>
2015-11-12 01:07:55 +0100

    Auto fill address

--- src/main/resources/public/resources/js/script-formulaire-offre.js
+++ src/main/resources/public/resources/js/script-formulaire-offre.js
@@ -1,40 +1,77 @@
-// Chargement du module "validationOffre"
+//Chargement du module "validationOffre"
 var validationApp = angular.module('validationOffre', ['ngMaterial', 'ngMessages', 'mdDateTime']);
 
 //On ajoute une directive qui va se charger de de contrôler l'expression régulière contenue dans le formulaire
 validationApp.directive('ensureExpression', ['$http', '$parse', function($http, $parse) {
 	return {
-	    require: 'ngModel',
-	    link: function(scope, ele, attrs, ngModelController) {
-	    scope.$watch(attrs.ngModel, function(value) {
-			// On calcule le résultat de l'expression
-			var resultat = $parse(attrs.ensureExpression)(scope);
-			// On informe la vue du résultat
-	    	ngModelController.$setValidity('mauvaisetranche', resultat);
-	    });
-	    }
+		require: 'ngModel',
+		link: function(scope, ele, attrs, ngModelController) {
+			scope.$watch(attrs.ngModel, function(value) {
+				// On calcule le résultat de l'expression
+				var resultat = $parse(attrs.ensureExpression)(scope);
+				// On informe la vue du résultat
+				ngModelController.$setValidity('mauvaisetranche', resultat);
+			});
+		}
 	};
 }]);
 
+
+
+
 //Création du controller "OffreCtrl"
 validationApp.controller('OffreCtrl', function($scope, $http, $window,$mdToast) {
-	   
+
+	$http.get('/utilisateur/particulier/profil').success(
+			function(donnees) {
+				// Quand on reçoit les données, on les envoie à la vue (stockage dans la variable profil)
+				$scope.profil = donnees;
+			}
+	);
+
+
+	$scope.homeAction = function() {
+
+		if($scope.home){
+
+			$scope.rue = $scope.profil.adresse.voie;
+			$scope.ville = $scope.profil.adresse.ville.nom;
+			$scope.cp = $scope.profil.adresse.ville.cp;
+			$scope.pays= $scope.profil.adresse.ville.cp.pays;
+
+		}else{
+
+			$scope.rue = '';
+			$scope.ville ='';
+			$scope.cp = '';
+			$scope.pays= '';
+		}
+
+
+	};
+
+
+
+
+
 	// On va rechercher toutes les types de cuisine en se connectant à la route consacrée
 	$http.get('/settings/typescuisines').success(
 			function(donnees) {
-				 $scope.cook = donnees;
-				 $scope.typeCuisine= $scope.cook[0].id;
+				$scope.cook = donnees;
+				$scope.typeCuisine= $scope.cook[0];
 			}
-		);
-	
+	);
+
+
+
 	// On va rechercher toutes les pays en se connectant à la route consacrée
 	$http.get('/settings/pays').success(
 			function(donnees) {
-				 $scope.count = donnees;
-				 $scope.pays= $scope.count[0].id;
+				$scope.count = donnees;
+				$scope.pays= $scope.count[0];
 			}
-		);
-	
+	);
+
 	// On pre-remplissage des champs 'durée', 'nbpers' et 'prix' par le minimum attendu
 	$scope.duree = 60;
 	$scope.prix = 1;
@@ -47,54 +84,54 @@ validationApp.controller('OffreCtrl', function($scope, $http, $window,$mdToast) 
 			var date_repas = new Date();
 			var aujourdhui = new Date();
 			var date = aujourdhui.toLocaleFormat('%Y-%m-%d');
-		    	
+
 			// On créé on objet pays
 			var pays = {
-				id : $scope.pays,
+					id : $scope.pays,
 			};
 
 			// On créé un objet ville
 			var ville = {
-				nom : $scope.ville,
-				cp : $scope.cp,
-				pays : pays,
+					nom : $scope.ville,
+					cp : $scope.cp,
+					pays : pays.id,
 			};
 
 			// On créé un objet adresse
 			var adresse = {
-				voie : $scope.numero + " " + $scope.rue + "" + $scope.complement,
-				ville : ville,
+					voie : $scope.numero + " " + $scope.rue + "" + $scope.complement,
+					ville : ville,
 			};
 
 			// On créé un objet pour le type de cuisine
 			var typeCuisine = {
-				id : $scope.typeCuisine,
+					id : $scope.typeCuisine.id,
 			};
-			
+
 			var menu = {
-				entree: $scope.entree,
-				plat: $scope.plat,
-				dessert: $scope.dessert,
-				boisson: $scope.boisson
+					entree: $scope.entree,
+					plat: $scope.plat,
+					dessert: $scope.dessert,
+					boisson: $scope.boisson
 			};
 
 			// Enfin on peut créer les données que l'on souhaite envoyer
-		    var donnees = {
-		    	dateCreation : date,
-		    	titre : $scope.titre,
-		    	prix : parseFloat($scope.prix),
-		    	nombrePersonne : parseInt($scope.nbpers),
-		    	dureeMinute : parseInt($scope.duree), // optionnel
-		    	dateRepas : date_repas.toISOString().substr(0,22),
-		    	note : $scope.note, //optionnel
-		    	menu : menu,
-		    	ageMin : parseInt($scope.agemin), //optionnel
-		    	ageMax : parseInt($scope.agemax), //optionnel
-		    	animaux : Boolean($scope.animal),
-		    	adresse : adresse,
-		    	typeCuisine : typeCuisine,
-		    };
-/*
+			var donnees = {
+					dateCreation : date,
+					titre : $scope.titre,
+					prix : parseFloat($scope.prix),
+					nombrePersonne : parseInt($scope.nbpers),
+					dureeMinute : parseInt($scope.duree), // optionnel
+					dateRepas : date_repas.toISOString().substr(0,22),
+					note : $scope.note, //optionnel
+					menu : menu,
+					ageMin : parseInt($scope.agemin), //optionnel
+					ageMax : parseInt($scope.agemax), //optionnel
+					animaux : Boolean($scope.animal),
+					adresse : adresse,
+					typeCuisine : typeCuisine,
+			};
+			/*
 		    // On envoie les données
             $http({
         		method: 'PUT',
@@ -107,8 +144,8 @@ validationApp.controller('OffreCtrl', function($scope, $http, $window,$mdToast) 
       		}).error(function(err, status, headers, config){
       			// DECLENCHEMENT D'UN TOASTER ICI : Erreur interne
      		});
-*/	
-		    console.log(donnees);
+			 */	
+			console.log(donnees);
 		}
 	};
 });
