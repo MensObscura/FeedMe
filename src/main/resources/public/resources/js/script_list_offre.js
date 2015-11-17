bd57c2ad70b7e9f9b4ebe0dedc7e6ecb2e257524
MensObscura <thibaultrosa@gmail.com>
2015-11-17 18:36:51 +0100

    nombre places offres

--- src/main/resources/public/resources/js/script_list_offre.js
+++ src/main/resources/public/resources/js/script_list_offre.js
@@ -7,7 +7,7 @@ app.controller("ListeCtrl", function($scope, $http, $window) {
 	$http.get('/offres').success(
 		function(donnees) {
 			$scope.list = donnees;
-						
+			$scope.nombrePlaces =0;	
 		}
 	);
     
@@ -15,7 +15,16 @@ app.controller("ListeCtrl", function($scope, $http, $window) {
 	$scope.visualize = function (valeur) {
 		   $window.location.href = "/offre.html?id="+valeur.id;
 	};
- 
+	//calcule le nombres de places restante
+	$scope.getNbPlaces = function (item) {
+		   var place_reservees = 0;
+			for (i = 0; i < item.reservations.length; i++) {
+				place_reservees +=  item.reservations[i].nbPlaces;
+			}
+			$scope.nombrePlaces = item.nombrePersonne - place_reservees;
+			item.nombrePersonne = $scope.nombrePlaces;
+				
+	};
 });
 
 
