6a81afbbcfa7f5d9f738f9febedca49747f3761f
Maxime CLEMENT <maxime.clement93@gmail.com>
2015-11-18 08:46:57 +0100

    [Web] Test credientials before redering view

--- src/main/resources/public/resources/js/script-index.js
+++ src/main/resources/public/resources/js/script-index.js
@@ -1,16 +1,41 @@
 // Chargement du module "Index"
-var app = angular.module("Index", []);
+var app = angular.module("Index", ['ngRoute']);
+
+app.config(
+		function($routeProvider, $httpProvider) {
+			$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+		});
 
 // Création du controller "MenuCtrl"
 app.controller("MenuCtrl", function($scope, $http) {
 
-	// On va se connecter sur la route permettant de récupèrer le profil de l'utilisateur
-	$http.get('/utilisateur/particulier/profil', {headers: { "Authorization" : "BasicCustom" }}).success(
-		function(donnees) {
-			// Si on reçoit les données, c'est que l'utilisateur est connecté, on n'affiche pas les boutons de connexion et d'inscription... par contre on peut se déconnecter
-			$scope.connecte = true;
-		}
-	).error(function (data, status){
-		$scope.connecte = false;
-    });
+	$scope.submited =false;
+	var authenticate = function(credentials, callback) {
+
+		var headers = credentials ? {
+			authorization : "Basic "
+				+ btoa(credentials.username + ":"
+						+ credentials.password)
+		} : {};
+
+		$http.get('user', {
+			headers : headers
+		}).success(function(data) {
+			if (data.name) {
+				$scope.authenticated = true;
+			} else {
+				$scope.authenticated = false;
+			}
+		}).error(function() {
+			$scope.authenticated = false;
+		});
+
+	}
+	
+	authenticate();
+	
+	$scope.credentials = {};
+	
+	
 });
