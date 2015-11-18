3b06ed33470c22350e9c1c9a3b960aca109fa477
Maxime CLEMENT <maxime.clement93@gmail.com>
2015-11-18 11:33:22 +0100

    [Image] DAO + TEST + MAPPER

---
+++ src/test/java/fil/iagl/iir/dao/ImageDaoTest.java
@@ -0,0 +1,83 @@
+package fil.iagl.iir.dao;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Test;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import fil.iagl.iir.entite.Image;
+import fil.iagl.iir.outils.SQLCODE;
+
+public class ImageDaoTest extends AbstractDaoTest {
+
+  @Test
+  public void sauvegarderTestSucces() throws Exception {
+    // Etant donne une image
+    Image image = new Image();
+    image.setPath("/monPath/1.jpg");
+
+    // Quand on enregistre cette image en base
+    imageDao.sauvegarder(image);
+
+    // Alors on verifie que l'ID est bien généré et set par Mybatis
+    Assertions.assertThat(image.getId()).isNotNull().isPositive();
+  }
+
+  @Test
+  public void sauvegarderTestEchec_PathNull() throws Exception {
+    // Etant donne une image avec un path null
+    Image image = new Image();
+
+    try {
+      // Quand on enregistre cette image en base
+      imageDao.sauvegarder(image);
+      // Alors on verifie que la base renvoie une exception
+      Assertions.fail("Doit soulever une exception");
+    } catch (DataIntegrityViolationException ex) {
+      // On verifie que l'exception est bien du au "NOT NULL"
+      assertSQLCode(ex, SQLCODE.NOT_NULL_VIOLATION);
+    }
+  }
+
+  @Test
+  public void sauvegarderTestEchec_PathUnique() throws Exception {
+    // Etant donne une image avec un path déjà existant
+    Image image = new Image();
+    image.setPath("/monPath/0.jpg");
+    try {
+      // Quand on enregistre cette image en base
+      imageDao.sauvegarder(image);
+      // Alors on verifie que la base renvoie une exception
+      Assertions.fail("Doit soulever une exception");
+    } catch (DataIntegrityViolationException ex) {
+      // On verifie que l'exception est bien du au "UNIQUE"
+      assertSQLCode(ex, SQLCODE.UNIQUE_VIOLATION);
+    }
+  }
+
+  @Test
+  public void getByIdTestSucces() throws Exception {
+    // Etant donnée les informations contenu dans la base
+    Integer id = 1;
+    String path = "/monPath/0.jpg";
+
+    // Quand je recupere l'image d'id 1
+    Image image = this.imageDao.getById(id);
+
+    // Alors on verifie que quelque chose est bien retourné
+    Assertions.assertThat(image).isNotNull();
+    // Alors on verifie que l'id correspond
+    Assertions.assertThat(image.getId()).isNotNull().isEqualTo(id);
+    // Alors on verifie que le path correspond
+    Assertions.assertThat(image.getPath()).isNotNull().isEqualTo(path);
+  }
+
+  @Test
+  public void getByIdTestEchec() throws Exception {
+    // On verifie que la base ne nous renvoi pas d'image lors que l'on demande celle d'id null
+    Assertions.assertThat(imageDao.getById(null)).isNull();
+
+    // On verifie que la base ne nous renvoi pas d'image lorsque celle ci n'est pas en base
+    Assertions.assertThat(imageDao.getById(Integer.MAX_VALUE)).isNull();
+  }
+
+}
