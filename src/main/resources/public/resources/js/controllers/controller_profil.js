79549c185f38f3a81ceb1d6018f0e4ddea3e028b
MensObscura <thibaultrosa@gmail.com>
2015-12-01 23:59:19 +0100

    Edit profil css, debut js

--- src/main/resources/public/resources/js/controllers/controller_profil.js
+++ src/main/resources/public/resources/js/controllers/controller_profil.js
@@ -1,7 +1,10 @@
 // Chargement du module "Profil"
-var app = angular.module("Profil",  ['ngAnimate']);
+var app = angular.module("Profil",  ['ngAnimate','ngMaterial', 'ngMessages']);
+
+
 
 app.controller("LogoutCtrl", function($scope, $http, $window) {
+    
 	// Fonction permettant une déconnexion :
 	$scope.logout = function () {
 		$http.get('/logout').success(
@@ -15,6 +18,11 @@ app.controller("LogoutCtrl", function($scope, $http, $window) {
 
 // Création du controller "ProfilCtrl"
 app.controller("ProfilCtrl", function($scope, $http) {
+	
+	//affichage des input d'edition du profil
+	$scope.editBio=false;
+	 
+    $scope.editAdr =false;
 	// On va se connecter sur la route permettant de récupèrer le profil de l'utilisateur
 	$http.get('/utilisateur/particulier/profil').success(
 		function(donnees) {
@@ -23,12 +31,107 @@ app.controller("ProfilCtrl", function($scope, $http) {
 		}
 	);
 	
+	// On va rechercher toutes les pays en se connectant à la route consacrée
+	$http.get('/settings/pays').success(
+			function(donnees) {
+				$scope.count = donnees;
+				$scope.saveCountry = $scope.count;
+			}
+	);
+
+	
 	  $scope.hoverIn = function(){
 	        this.hoverEdit = true;
 	     };
 
+	     
 	    $scope.hoverOut = function(){
 	        this.hoverEdit = false;
 	    };
+	    
+	    //on affiche l'édition de la bio
+	    $scope.setEditBio = function(){
+
+	        $scope.editBio = true 
+	    };
+	    
+	    //on affiche l'édition de l'addresse
+	    $scope.setEditAdr = function(){
+	        $scope.editAdr = true ;
+	    	   $scope.homeAction();
+	    
+	        
+	    };
+	    
+	    
+	    // decoupe l'adresse si exsistante
+	    $scope.homeAction = function() {
+	    	
+			if($scope.editAdr){
+
+				var rue = $scope.profil.adresse.voie;
+				var num = rue.split(" ")[0];
+				
+				$scope.numero = parseInt(num);
+				$scope.rue = rue.substring(num.length+1,rue.length);
+				$scope.ville = $scope.profil.adresse.ville.nom;
+				$scope.cp = $scope.profil.adresse.ville.cp;
+				$scope.count= {0: $scope.profil.adresse.ville.pays};
+
+			}else{
+				$scope.numero = '';
+				$scope.rue = '';
+				$scope.ville ='';
+				$scope.cp = '';
+				$scope.count = $scope.saveCountry;
+			}
+
+
+		};
+		
+		// propose tout les pays
+		$scope.allCountry = function(){
+			
+			$scope.count = $scope.saveCountry;
+		};
+		
+		
+		
+		 $scope.submitEdition = function(){
+			 if ($scope.ProfilForm.$valid){
+				 
+				// On créé on objet pays
+					var pays = {
+							id : $scope.pays,
+					};
+
+					// On créé un objet ville
+					var ville = {
+							nom : $scope.ville,
+							cp : $scope.cp,
+							pays : pays.id,
+					};
+					//build de l'adresse
+					var adresse = {
+							voie : $scope.numero + " " + $scope.rue + "" + $scope.complement,
+							ville : ville,
+					};
+					
+					var donnee = {
+							adresse : adresse,
+							description : profil.description,
+							image : null
+							
+							
+					}
+			
+				 
+				 console.log('valid');
+				 
+				 
+				 
+			 }
+		 };
+	    
 	
 });
