ff40bef091a10fc0b24d752bf2a49b5cf47d7c39
Pauline Dessingue <dessingue.pauline@gmail.com>
2016-01-06 10:00:23 +0100

    [BDD] Creation table VOTE

---
+++ bdd/scripts/20160106094556_create_table_vote.sql
@@ -0,0 +1,15 @@
+-- // create_table_vote
+-- Migration SQL that makes the change goes here.
+CREATE TABLE VOTE (
+	vot_id SERIAL PRIMARY KEY,
+	vot_usr_id INTEGER NOT NULL REFERENCES UTILISATEUR(usr_id),
+	vot_off_id INTEGER NOT NULL REFERENCES OFFRE(off_id),
+	vot_note INTEGER NOT NULL CONSTRAINT vote_valide CHECK (vot_note > 0 AND vot_note < 6),
+	CONSTRAINT tuple_unique_vote UNIQUE (vot_usr_id, vot_off_id)
+);
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+DROP TABLE VOTE;
+
