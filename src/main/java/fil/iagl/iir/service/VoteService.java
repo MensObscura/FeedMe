c4f97ddd018a42190f148d822170f52d9bc43869
Pauline Dessingue <dessingue.pauline@gmail.com>
2016-01-07 22:10:35 +0100

    feat(Vote): Annotation magique qui fait tout

--- src/main/java/fil/iagl/iir/service/VoteService.java
+++ src/main/java/fil/iagl/iir/service/VoteService.java
@@ -2,8 +2,11 @@ package fil.iagl.iir.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import fil.iagl.iir.entite.Vote;
 
+@Service
 public interface VoteService {
 
   void sauvegarder(Vote vote);
